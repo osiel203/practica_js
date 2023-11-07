f0f549974cc6e24985f655ced8946940002683f3
jaime osiel santiago guzman <132520862+osiel203@users.noreply.github.com>
2023-11-06 18:10:11 -0600

    Add files via upload

---
+++ practicajs/operaciones.js
@@ -0,0 +1,36 @@
+function suma(){
+    let n1, n2, suma
+    n1 = prompt("introduce un numero")
+    n2 = prompt("introduce un numero")
+
+    suma = parseInt(n1) + parseInt(n2)
+    alert(suma)
+}
+
+function restar() {
+    let n1, n2, restar
+    n1 = prompt("introduce un numero")
+    n2 = prompt("introduce un numero")
+
+    restar = parseInt(n1) - parseInt(n2)
+    alert(restar)
+
+}
+
+function multiplicar() {
+    let n1, n2, multiplicar
+    n1 = prompt("introduce un numero")
+    n2 = prompt("introduce un numero")
+
+    multiplicar = parseInt(n1) * parseInt(n2)
+    alert(multiplicar)
+}
+
+function dividir() {
+    let n1, n2, resta
+    n1 = prompt("introduce un numero")
+    n2 = prompt("introduce un numero")
+
+    dividir = parseInt(n1) / parseInt(n2)
+    alert(dividir)
+}
